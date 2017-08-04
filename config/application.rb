da7591056638865cfb9e6ab23cb7f50d8c046eef
Atsuco Miyoshi <a.miyoshi3@icloud.com>
2017-08-04 17:53:08 +0900

    プロフィールページの追加

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,8 @@ Bundler.require(*Rails.groups)
 module Huggy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    # config.i18n.default_locale = :de のコメントアウトがあるのでこれを利用すればよい
+    config.i18n.default_locale = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-
   root :to => 'pages#index'
 
+  devise_for :users
+
+  resources :users, only:[:show]
 
 end
