a49ae202324774527f6876adfa5f0a9623ca8154
Atsuco Miyoshi <a.miyoshi3@icloud.com>
2017-08-08 21:04:39 +0900

    プロフィール内容を編集するページを作成2

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root :to => 'pages#index'
 
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => 'registrations'}
+
 
   resources :users, only:[:show]
-
 end
